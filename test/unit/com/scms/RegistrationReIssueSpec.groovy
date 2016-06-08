c75ccabb8e63ef3f5447faec516f9bfad867172c
tonmoy <tonmoy@friendship-bd.org>
2016-06-08 11:09:08 +0600

    re issue domain

--- test/unit/com/scms/RegistrationReIssueSpec.groovy
+++ test/unit/com/scms/RegistrationReIssueSpec.groovy
@@ -6,8 +6,8 @@ import spock.lang.Specification
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@TestFor(RegistrationReIssue)
-class RegistrationReIssueSpec extends Specification {
+@TestFor(RegistrationReissue)
+class RegistrationReissueSpec extends Specification {
 
     def setup() {
     }
