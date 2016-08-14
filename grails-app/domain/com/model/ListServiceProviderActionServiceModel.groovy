670c869935418c58e6a272b3a1a39609e7d60626
zakariaimtiaz <imtiaz71985@gmail.com>
2016-08-14 13:09:35 +0600

    add service provider feature

--- grails-app/domain/com/model/ListServiceProviderActionServiceModel.groovy
+++ grails-app/domain/com/model/ListServiceProviderActionServiceModel.groovy
@@ -4,6 +4,8 @@ class ListServiceProviderActionServiceModel {
 
     public static final String MODEL_NAME = 'list_service_provider_action_service_model'
     public static final String SQL_LIST_SERVICE_PROVIDER_MODEL = """
+            CREATE OR REPLACE VIEW list_service_provider_action_service_model AS
+
             SELECT sp.id, sp.version, se.id AS type_id, se.name AS type_name,sp.name,sp.is_active,sp.mobile_no
                   FROM service_provider sp
                   LEFT JOIN system_entity se ON se.id=sp.type_id AND se.type = 'Service Provider';
