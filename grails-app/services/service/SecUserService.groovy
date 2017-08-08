07e1fb9a4695550aea0d7184a5776a4ab623c2a0
tonmoy <tonmoy@friendship-bd.org>
2017-08-08 13:18:03 +0600

    small change

--- grails-app/services/service/SecUserService.groovy
+++ grails-app/services/service/SecUserService.groovy
@@ -13,7 +13,7 @@ class SecUserService extends BaseService {
 
     public boolean isLoggedUserAdmin(long userId) {
         SecUser user = SecUser.read(userId)
-        List<SecRole> roleAdmin = SecRole.findAllByAuthorityOrAuthorityOrAuthority("ROLE_ADMIN", "ROLE_REPORT_VIEWER", "ROLE_HO_ADMIN")
+        List<SecRole> roleAdmin = SecRole.findAllByAuthorityOrAuthorityOrAuthority("ROLE_SCMS_ADMIN", "ROLE_SCMS_REPORT_VIEWER", "ROLE_SCMS_HO_ADMIN")
         //SecRole roleHOSup = SecRole.findByAuthorityOrAuthority("ROLE_REPORT_VIEWER","ROLE_HO_ADMIN")
         int count =0
         for (SecRole secRole : roleAdmin) {
